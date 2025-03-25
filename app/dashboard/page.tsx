dddc4edd6c0c5b206b5763c9a49fe03ffdf105d2
praveens649 <praveenv649@gmail.com>
2025-03-25 14:22:50 +0530

    feat:implemented herosection

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import DashboardCards from './_components/dashboard-cards'
+import { HeroSection } from './_components/dashboard-cards'
+
 
 const page = () => {
   return (
     <div>
-      <DashboardCards/>
+      <HeroSection/>
     </div>
   )
 }
